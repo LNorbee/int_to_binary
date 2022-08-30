b09f8310bcfda21201993bc0a1280fa9724c0347
LNorbee <lazar.norbeert@gmail.com>
2022-08-30 13:14:58 +0200

    Modified
    - Rename "recursion" method to "inputNumberToBinary".
    - Add "toBinary" to Methods.
    - Upgrade the data request check.
    - Change try-with-resources back to try-catch.
    - Add finally for try-catch.

--- src/Main/Methods.java
+++ src/Main/Methods.java
@@ -3,27 +3,37 @@ package Main;
 import java.util.Arrays;
 
 public class Methods {
-	protected int recursion(int num) {
+	
+	protected void toBinary(int num) {
+		System.out.println("\n" + "Bináris tömbként: ");
+
+		char[] s = Integer.toBinaryString(num).toCharArray();
+
+		System.out.println(s);			
+		System.out.println();
+	}
+	
+	protected int inputNumberToBinary(int num) {
 		// int[] binary = { 1, 2, 4, 8, 16, 32, 64, 128 };
 		int[] binary = { 128, 64, 32, 16, 8, 4, 2, 1 };
 		int[] result = new int[binary.length];
-
+		
 		int num2 = 0;
 
 		for (int i = 0; i < binary.length; i++) {
 			if (num / binary[i] == 1) {
 				num2 = num % binary[i];
 				result[i] = 1;
+				System.out.println(Arrays.toString(result));
 			}
 		}
-		System.out.println(Arrays.toString(result));
 		if (num2 <= 0) {
 			return 0;
 		}
-		return recursion(num2);
+		return inputNumberToBinary(num2);
 	}
 
-	protected int recursion2(int num) {
+	protected int inputNumberToBinary_2(int num) {
 
 		if (num % 2 == 0) {
 			System.out.print(0 + " ");
@@ -37,6 +47,6 @@ public class Methods {
 		if (num <= 0) {
 			return 0;
 		}
-		return recursion2(num);
+		return inputNumberToBinary_2(num);
 	}
 }
--- src/Main/To_Binary.java
+++ src/Main/To_Binary.java
@@ -6,46 +6,44 @@ import java.util.Scanner;
 public class To_Binary {
 
 	public static void main(String[] args) {
+		
+		//TODO add GUI / User surface.
+		Methods method = new Methods();
 
 		System.out.print("Adj meg egy számot 1-255 között: ");
-		Scanner scan = null;
-		try {
-			scan = new Scanner(System.in);
-			int num = scan.nextInt();
-
-			// Bevitt szám ellenőrzése hogy 1-255 közt legyen
-			while (num < 1 || num > 255) {
-				System.out.print("Nem megfelelő, add meg újra: ");
-				num = scan.nextInt();
+		
+		boolean inputCorrect;
+		Scanner scan = new Scanner(System.in);
+		do {
+			inputCorrect = true;
+			try {
+				int num = scan.nextInt();
+				
+				// Bevitt szám ellenőrzése hogy 1-255 közt legyen
+				while (num < 1 || num > 255) {
+					System.out.print("1 - 255 közötti számot adj meg: ");
+					num = scan.nextInt();
+				}
+				
+				method.toBinary(num);
+				
+				method.inputNumberToBinary(num);
+				
+			} catch (InputMismatchException e) {
+				System.out.print("Nem megfelelő input, add meg újra: ");
+				inputCorrect = false;
+			} finally {
+				scan.nextLine();
 			}
+		} while(!inputCorrect);
+		scan.close();
 
-			System.out.println("\n" + "Bináris tömbként: ");
-
-			char[] s = Integer.toBinaryString(num).toCharArray();
-			System.out.println(s);
-			System.out.println();
-
-			Methods method = new Methods();
-			method.recursion(num);
-			// System.out.println();
-			// method.recursion2(num);
-
-			// 1 Byte, és kiíratása
-			int[] binary = { 1, 2, 4, 8, 16, 32, 64, 128 };
-
-			System.out.println("\n" + "1 byte: ");
-			for (int i = binary.length - 1; i >= 0; i--) {
-				System.out.print(binary[i] + ", ");
-			}
-
-		} catch (InputMismatchException e) {
-			System.out.println("Nem megfelelő input!");
-
-		} finally {
-			if (scan != null) {
-				scan.close();
-			}
+		// 1 Byte, és kiíratása
+		int[] binary = { 1, 2, 4, 8, 16, 32, 64, 128 };
+		
+		System.out.println("\n" + "1 byte: ");
+		for (int i = binary.length - 1; i >= 0; i--) {
+			System.out.print(binary[i] + ", ");
 		}
-		System.out.println(scan);
 	}
 }
